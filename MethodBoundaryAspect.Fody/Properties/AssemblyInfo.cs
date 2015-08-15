35534104ed6d1c92cb5975ee0ee1e510f0c73ef5
Marcell Spies <marcells@gmx.de>
2015-08-15 17:09:48 +0200

    Reuse assembly information for nuget package.

--- MethodBoundaryAspect.Fody/Properties/AssemblyInfo.cs
+++ MethodBoundaryAspect.Fody/Properties/AssemblyInfo.cs
@@ -1,17 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MethodBoundaryAspect.Fody")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fody add-in to decorate arbitrary methods to run code before, after invocation and on exception")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("VESCON GmbH")]
 [assembly: AssemblyProduct("MethodBoundaryAspect.Fody")]
 [assembly: AssemblyCopyright("VESCON GmbH 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("VESCON GmbH")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f6187df1-e3d2-4d26-add6-1eba416dee74")]
 [assembly: AssemblyVersion("0.3.5")]
