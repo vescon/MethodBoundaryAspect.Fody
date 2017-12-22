da568c3a9dca99a06a4501aae878e880ccb8cfa0
r.kornelius <r.kornelius@vescon.com>
2017-12-22 16:36:04 +0100

    added unittest to ensure 'Overriding OnException without overriding OnEntry results in a NullReferenceException #8' is fixed

---
+++ MethodBoundaryAspect.Fody.UnitTests.TestAssembly/Aspects/OnlyOnExceptionAspect.cs
@@ -0,0 +1,11 @@
+﻿using MethodBoundaryAspect.Fody.Attributes;
+
+namespace MethodBoundaryAspect.Fody.UnitTests.TestAssembly.Aspects
+{
+    public class OnlyOnExceptionAspect : OnMethodBoundaryAspect
+    {
+        public override void OnException(MethodExecutionArgs arg)
+        {
+        }
+    }
+}
---
+++ MethodBoundaryAspect.Fody.UnitTests/SpecialAspectTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using FluentAssertions;
+using MethodBoundaryAspect.Fody.UnitTests.TestAssembly;
+using NUnit.Framework;
+
+namespace MethodBoundaryAspect.Fody.UnitTests
+{
+    public class SpecialAspectTests : MethodBoundaryAspectTestBase
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+
+        [Test]
+        public void IfMethodWithAspectOnEntryOnlyIsWeaved_ThenTheAssemblyShouldBeValid()
+        {
+            // Arrange
+            const string testMethodName = "MethodWithAspectOnEntryOnly";
+            var testClassType = typeof(SpecialAspectMethods);
+
+            // Act
+            WeaveAssemblyClassAndLoad(testClassType);
+            Action call = () => AssemblyLoader.InvokeMethod(testClassType.FullName, testMethodName);
+
+            // Assert
+            call.ShouldNotThrow();
+        }
+
+        [Test]
+        public void IfMethodWithAspectOnExitOnlyIsWeaved_ThenTheAssemblyShouldBeValid()
+        {
+            // Arrange
+            const string testMethodName = "MethodWithAspectOnExitOnly";
+            var testClassType = typeof(SpecialAspectMethods);
+
+            // Act
+            WeaveAssemblyClassAndLoad(testClassType);
+            Action call = () => AssemblyLoader.InvokeMethod(testClassType.FullName, testMethodName);
+
+            // Assert
+            call.ShouldNotThrow();
+        }
+
+        [Test]
+        public void IfMethodWithAspectOnExceptionOnlyIsWeaved_ThenTheAssemblyShouldBeValid()
+        {
+            // Arrange
+            const string testMethodName = "MethodWithAspectOnExceptionOnly";
+            var testClassType = typeof(SpecialAspectMethods);
+
+            // Act
+            WeaveAssemblyClassAndLoad(testClassType);
+            Action call = () => AssemblyLoader.InvokeMethod(testClassType.FullName, testMethodName);
+
+            // Assert
+            call.ShouldNotThrow();
+        }
+    }
+}
