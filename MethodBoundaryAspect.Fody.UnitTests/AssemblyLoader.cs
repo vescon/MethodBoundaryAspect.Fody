39fbe62355af22e33927f1931f137e52e48b995b
Waut Van den Abbeele <wautvda@gmail.com>
2018-06-04 13:10:16 +0200

    undo change

--- MethodBoundaryAspect.Fody.UnitTests/AssemblyLoader.cs
+++ MethodBoundaryAspect.Fody.UnitTests/AssemblyLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -10,7 +11,7 @@ namespace MethodBoundaryAspect.Fody.UnitTests
 
         public void Load(string assemblyPath)
         {
-            _assembly = Assembly.LoadFrom(assemblyPath);
+            _assembly = Assembly.Load(File.ReadAllBytes(assemblyPath));
         }
 
         public object InvokeMethodWithResultClass(
