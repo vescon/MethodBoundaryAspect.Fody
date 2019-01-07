78460ffce0a886feaae5c46110aa64315d890c16
martinay <4411119+Martinay@users.noreply.github.com>
2019-01-07 16:43:02 +0100

    #52 fixed dotnet sample

--- Samples/HelloWorld_NetCore/HelloWorld_NetCore/Program.cs
+++ Samples/HelloWorld_NetCore/HelloWorld_NetCore/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using HelloWorld_NetFramework_Attributes;
 
 namespace HelloWorld_NetCore
 {
-    class Program
+    [HelloWorldAspect]
+    static class Program
     {
+        /// <summary>
+        /// Output:
+        /// 
+        /// Entered method: Main
+        /// Entered method: PrintHelloWorld
+        /// Hello World
+        /// Exited method: PrintHelloWorld
+        /// Exited method: Main
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintHelloWorld();
+        }
+
+        private static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
         }
     }
 }
